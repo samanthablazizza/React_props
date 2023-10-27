55a3f1a36f612926c91a5a499fb78a3ebc6c3ced
samanthablazizza <samantha.blazizza@gmail.com>
2023-10-26 21:21:06 -0300

    Props, Hooks e Renderização Condicional no React

---
+++ layout-basico/src/components/card/Card.tsx
@@ -0,0 +1,19 @@
+interface CardProps{
+    titulo: string
+    descricao: string
+}
+
+function Card({ titulo, descricao }: CardProps) {
+  return (
+    <div style={{
+        width: "500px",
+        border: "5px solid crimson",
+        margin: "0 0 1rem 0"
+    }}>
+        <h1>{ titulo }</h1>
+        <p>{ descricao }</p>
+    </div>
+  )
+}
+
+export default Card
